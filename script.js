8bb7b6c067867c691bf17b7357abbd5d05dd73d1
muhammed-emir-guncu <70435490+muhammed-emir-guncu@users.noreply.github.com>
2020-09-09 19:22:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+var a=["1","2","3","4","5","6"];
+document.getElementById("text1").innerHTML = a[Math.floor(Math.random()*5)];
+document.getElementById("text2").innerHTML = a[Math.floor(Math.random()*5)];
+document.getElementById("text3").innerHTML = a[Math.floor(Math.random()*5)];
