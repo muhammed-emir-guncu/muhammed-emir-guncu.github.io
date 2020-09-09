d52ca4feb28ef21430e8687b3172c9e613f0d654
muhammed-emir-guncu <70435490+muhammed-emir-guncu@users.noreply.github.com>
2020-09-10 00:30:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var a=["1","2","3","4","5","6"];
-
-document.getElementById("text1").innerHTML = a[Math.floor(Math.random()*5)];
-document.getElementById("text2").innerHTML = a[Math.floor(Math.random()*5)];
-document.getElementById("text3").innerHTML = a[Math.floor(Math.random()*5)];
+function bas(){
+document.getElementById("text1").innerHTML = a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)];
+document.getElementById("text2").innerHTML = a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)];
+document.getElementById("text3").innerHTML = a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)];
+};
+bas();
