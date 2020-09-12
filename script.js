b3e0ebacbfd70e51ae1f8a83f3ee0c15f55d4414
muhammed-emir-guncu <70435490+muhammed-emir-guncu@users.noreply.github.com>
2020-09-12 14:36:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var a=["1","2","3","4","5","6"];
+var a=["nasa","duz dunya","kure dunya","kuantum","fizik","yasam","nasa"];
+var b=["guzel","kotu","mantikli","mantiksiz","cirkin"];
+var c=["gercek","yalan","duzmece","sey"];
 function bas(){
-document.getElementById("text1").innerHTML = a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)];
-document.getElementById("text2").innerHTML = a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)];
-document.getElementById("text3").innerHTML = a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)]+" : "+a[Math.floor(Math.random()*5)];
+document.getElementById("text").innerHTML =a[Math.floor(Math.random() * a.length)]+" "+b[Math.floor(Math.random() * b.length)]+" bir "+c[Math.floor(Math.random() * c.length)] ;
 };
 bas();
